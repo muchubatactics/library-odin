73d35556cf4f1a26ea49dff4606fa3fba9b1dc4c
muchubatactics <byooxmckeith@gmail.com>
2023-10-19 14:44:09 +0300

    basic design complete

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ function Book(title, author, pages, isRead)
     this.title = title;
     this.author = author;
     this.numberOfPages = pages;
-    this.isRead = isRead;
-    this.info = function(){
-        let str = `${this.title} by ${this.author}, ${this.numberOfPages} pages, `;
-        if (this.isRead) str += "already read.";
-        else str += "not read yet.";
-        return str;
-    } 
+    this.isRead = isRead; 
+}
+
+Book.prototype.info = function(){
+    let str = `${this.title} by ${this.author}, ${this.numberOfPages} pages, `;
+    if (this.isRead) str += "already read.";
+    else str += "not read yet.";
+    return str;
+}
+
+function addBookToLibrary(title, author, pages, isRead)
+{
+    myLibrary.push(new Book(title, author, pages, isRead));
 }
